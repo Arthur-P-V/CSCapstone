197adb44c396ce54d74fde055d91968f23322461
kaliuser876 <jonrrich112@gmail.com>
2025-03-20 11:31:36 -0500

    Changed the message of the response

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Hello via Bun!");
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        return new Response("Hello World!");
+        return new Response("Evil Things are coming yoru way!");
     },
 });
 
