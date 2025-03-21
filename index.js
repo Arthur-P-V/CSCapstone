783da619b2935574cb4403445749ab4348755ac0
kaliuser876 <jonrrich112@gmail.com>
2025-03-21 13:21:18 -0500

    On the JonBranch?

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ console.log("Hello via Bun!");
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        return new Response("Hello World!");
+        return new Response("Jon Branch?");
     },
 });
 
