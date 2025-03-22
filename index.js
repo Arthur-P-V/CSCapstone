350649a9682950f7938345486bdf9a99ebbb9260
kaliuser876 <jonrrich112@gmail.com>
2025-03-22 01:39:41 -0500

    Change from home

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-console.log("Hello via Bun!");
+console.log("Hello via Jon Desktop!");
 
 const server = Bun.serve({
     port: 3000,
     fetch(req) {
-        return new Response("Jon Branch?");
+        return new Response("Welcome to the Jon Branch, we have cookies!");
     },
 });
 
