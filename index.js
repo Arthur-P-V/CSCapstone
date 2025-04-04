cb54675f83c0f5a2634d2b3b9c206902a44cbdb8
Arthur-P-V <apetersonveatch@gmail.com>
2025-04-04 17:04:58 -0500

    WIP GET route for events

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { drizzle } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import { events } from "./db/schema/events";
+import { json } from "drizzle-orm/mysql-core";
 
 const connection = await mysql.createConnection({
   host: process.env.HOST,
@@ -16,6 +17,17 @@ const response = await db.select().from(events);
 console.log("Hello via Bun!");
 
 const server = Bun.serve({
+
+    routes: {
+
+        "/api/events": {
+            GET: async () => {
+                const data = await db.select().from(events)
+                new Response(json(data))
+            }
+        }
+    },
+
     port: 3000,
     fetch(req) {
         return new Response("You are now on the EVIL BRANCH!");
