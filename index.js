f70fc56bac6f8c921a26da2966542ddad588ef4e
kaliuser876 <jonrrich112@gmail.com>
2025-05-06 01:53:59 -0500

    Made the first page of front end connect to the server. The next step is creating all the html pages, then calling the proper functions when needed.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import { eq, ne, gt, gte} from "drizzle-orm";
 import {create_class, delete_class, get_all_classes, get_class_by_id} from "./functions/class_functions";
 import { get_all_users, get_user_by_eNumber, delete_user, create_user } from "./functions/user_functions";
 
+import app from "./front_end/index.html"
+
 const connection = await mysql.createConnection({
   host: process.env.HOST,
   user: process.env.USER,
@@ -23,9 +25,10 @@ console.log("Hello via Bun!");
 const server = Bun.serve({
 
     routes: {
-      // Different routes, currently have users and events
+    
+        // Different routes, currently have users and events
       // Loads all the users or create a new user
-      "/api/users": {
+        "/api/users": {
         GET: async () => {
           const data = await get_all_users(db);
           return Response.json(data);
@@ -34,9 +37,9 @@ const server = Bun.serve({
           const data = await create_user(db, req);
           return Response.json(data);
       }
-      },
-      // Searches up by enumber
-      "/api/users/:eNumber":{
+        },
+        // Searches up by enumber
+        "/api/users/:eNumber":{
         // Get user by eNumber
         GET: async req => {
           const data = await get_user_by_eNumber(db, req);
@@ -51,8 +54,8 @@ const server = Bun.serve({
        PUT: async req => {
            return new Response("UPDATE UPDATE");
        }
-      },
-      // Loads all the events or create a new event
+        },
+        // Loads all the events or create a new event
 
         "/api/classes": {
             GET: async () => {
@@ -82,7 +85,49 @@ const server = Bun.serve({
             PUT: async req => {
                 return new Response("UPDATE UPDATE");
             }
-        }
+        },
+
+        // The front end, this is using functions in the "front_end" folder and creating front end pages.
+        // check-in is the default page
+        "/check-in":{
+            GET: async (req) =>{
+                const html = await Bun.file("front_end/index.html").text();
+                return new Response(html, {
+                    headers: {
+                        "Content-Type": "text/html",
+                    },
+                });
+            },
+        },
+        "/styles.css": {
+      GET: async (req) => {
+        const file = Bun.file("front_end/styles.css");
+    if (!(await file.exists())) {
+      return new Response("CSS file not found", { status: 404 });
+    }
+        
+        return new Response(file, {
+          headers: {
+            "Content-Type": "text/css",
+          },
+        });
+      },
+    },
+
+    "/EU-PIC.jpg": {
+      GET: async (req) => {
+        const jpg = Bun.file("front_end/EU-PIC.jpg");
+    if (!(await jpg.exists())) {
+      return new Response("Image not found", { status: 404 });
+    }
+        return new Response(jpg, {
+          headers: {
+            "Content-Type": "image/jpeg",
+          },
+        });
+      },
+    },
+    
     },
 
     port: 3000,
